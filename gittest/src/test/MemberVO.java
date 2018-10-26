c1ffa6e7d65b3b0dc9bd79144fed0d8d75ba1095
weenie82 <shin910802@naver.com>
2018-10-26 15:08:18 +0900

    Vo 추가

---
+++ gittest/src/test/MemberVO.java
@@ -0,0 +1,21 @@
+package test;
+
+public class MemberVO {
+	private String no;
+	private String age;
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
+
+}
